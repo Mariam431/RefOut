fa7cccc08a719e8993e6d7a7d0b0cecf0f657920
mariam <mariam@MYPROBOOK>
2023-02-20 15:43:46 +0400

    Add project files.

---
+++ Hpmework20.02.23/RefOut.cs
@@ -0,0 +1,40 @@
+﻿namespace Hpmework20._02._23
+{
+    internal class RefOut
+    {
+        //public static string GetNextName(ref int id)
+        //{
+        //    string returnText = "Next-" + id.ToString();
+        //    id += 1;
+        //    return returnText;
+        //}
+        //static void Main(string[] args)
+        //{
+        //    int i = 1;
+        //    Console.WriteLine("Previous value of integer i:" + i.ToString());
+        //    string test = GetNextName(ref i);
+        //    Console.WriteLine("Current value of integer i:" + i.ToString());
+        //}
+
+
+
+        //public static string GetNextNameByOut(out int id)
+        //{
+        //    id = 1;
+        //    string returnText = "Next-" + id.ToString();
+        //    return returnText;
+        //}
+        //static void Main(string[] args)
+        //{
+        //    int i = 0;
+        //    Console.WriteLine("Previous value of integer i:" + i.ToString());
+        //    string test = GetNextNameByOut(out i);
+        //    Console.WriteLine("Current value of integer i:" + i.ToString());
+        //}
+
+    }
+}
+
+
+
+
